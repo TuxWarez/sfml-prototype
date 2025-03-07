30a25b0254d3f677660a11d0a12a6f3b471c247e
TuxWare <71888789+TuxWarez@users.noreply.github.com>
2025-03-07 18:19:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include "game.h"
 using namespace std;
 using namespace sf;
 
 int main() {
-    RenderWindow window(VideoMode(640, 480), "Tuxware's game", Style::Titlebar | Style::Close);
-    Event ev;
+    Game game;
 
-    while (window.isOpen()) {
-        while (window.pollEvent(ev)) {
-            switch (ev.type) {
-            case Event::Closed:
-                window.close();
-                break;
-            case Event::KeyPressed:
-                if (ev.key.code == Keyboard::Escape) {
-                    window.close();
-                }
-                break;
-            }
-        }
-        window.clear(Color(0xAA, 0xAA, 0xAA, 255));
-
-        window.display();
+    while (game.running()) {
+        game.update();
+        game.render();
     }
 
     return 0;
