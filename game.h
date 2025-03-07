7ee76ccf9cb9d921b929aa888be5d5fa6ba7ffbb
TuxWare <71888789+TuxWarez@users.noreply.github.com>
2025-03-07 18:18:39 +0100

    Update game.h

--- game.h
+++ game.h
@@ -1,14 +1,25 @@
 #pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
 
 class Game {
 private:
+    RenderWindow* window;
+    VideoMode videoMode;
+    Event ev;
 
-
+    void initVariables();
+    void initWindow();
 public:
     Game();
-    virtual ~Game;
+    virtual ~Game();
+    //accessors
+    const bool running() const;
 
     //funcions
+    void pollEvents();
     void update();
     void render();
-}
+};
