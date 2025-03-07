3d3c2d4d39799f4f94d3a60065f28bf3d63261c4
TuxWare <71888789+TuxWarez@users.noreply.github.com>
2025-03-07 18:20:54 +0100

    Update game.h

--- game.h
+++ game.h
@@ -23,3 +23,29 @@ public:
     void update();
     void render();
 };
+
+/*
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int n = 1;
+    int f = 0;
+    int i;
+    while (n < 2100000000) {
+        n += 2;
+        if (n % 3 == 0)
+            f = 1;
+        for (i = 5; i <= sqrt(n); i = i + 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                f = 1;
+            }
+        }
+        if (f == 0) {
+            cout << n << endl;
+        }
+        f = 0;
+    }
+}
+*/
